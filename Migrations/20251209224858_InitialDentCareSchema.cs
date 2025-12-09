e4738da01d95f643956183f4ed3b7db8debeef24
jose.fernando <jose.marcial@nepen.org.br>
2025-12-09 18:57:48 -0400

    add initial database schema for DentCare application

---
+++ Migrations/20251209224858_InitialDentCareSchema.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DentCare.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDentCareSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dentistas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NomeCompleto = table.Column<string>(type: "TEXT", nullable: false),
+                    CRO = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dentistas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pacientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NomeCompleto = table.Column<string>(type: "TEXT", nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Cpf = table.Column<string>(type: "TEXT", nullable: false),
+                    Telefone = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pacientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tratamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    Preco = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tratamentos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Consultas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DataHora = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", nullable: false),
+                    PacienteId = table.Column<int>(type: "INTEGER", nullable: false),
+                    DentistaId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consultas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Consultas_Dentistas_DentistaId",
+                        column: x => x.DentistaId,
+                        principalTable: "Dentistas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Consultas_Pacientes_PacienteId",
+                        column: x => x.PacienteId,
+                        principalTable: "Pacientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ConsultasTratamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ConsultaId = table.Column<int>(type: "INTEGER", nullable: false),
+                    TratamentoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Quantidade = table.Column<int>(type: "INTEGER", nullable: false),
+                    PrecoUnitario = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConsultasTratamentos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ConsultasTratamentos_Consultas_ConsultaId",
+                        column: x => x.ConsultaId,
+                        principalTable: "Consultas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ConsultasTratamentos_Tratamentos_TratamentoId",
+                        column: x => x.TratamentoId,
+                        principalTable: "Tratamentos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consultas_DentistaId",
+                table: "Consultas",
+                column: "DentistaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consultas_PacienteId",
+                table: "Consultas",
+                column: "PacienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConsultasTratamentos_ConsultaId",
+                table: "ConsultasTratamentos",
+                column: "ConsultaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConsultasTratamentos_TratamentoId",
+                table: "ConsultasTratamentos",
+                column: "TratamentoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ConsultasTratamentos");
+
+            migrationBuilder.DropTable(
+                name: "Consultas");
+
+            migrationBuilder.DropTable(
+                name: "Tratamentos");
+
+            migrationBuilder.DropTable(
+                name: "Dentistas");
+
+            migrationBuilder.DropTable(
+                name: "Pacientes");
+        }
+    }
+}
